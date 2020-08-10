ae0742c504fe93fe7f04d05d63551b6cd071e089
karimLa <kira00luffy@gmail.com>
2020-08-10 20:08:28 +0100

    first commit

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import next from 'next';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    // tslint:disable-next-line:no-console
+    console.log(
+      `> Ready on http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`
+    );
+  });
+});
